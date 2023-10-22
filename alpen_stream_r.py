014e12ea74a6ab1d4718be5d1f9bcb2df708f6c5
sarastefani <81649211+sarastefani@users.noreply.github.com>
2023-10-22 21:23:20 +0200

    Update alpen_stream_r.py

--- alpen_stream_r.py
+++ alpen_stream_r.py
@@ -93,7 +93,7 @@ def clear_input():
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
 # initialize pinecone
-index_name = "alpen"
+index_name = "alpen2"
 #index_name = "chatbot"
 
 pinecone.init(
