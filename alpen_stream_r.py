421f546fd50d5142833d95722a6c146de18b2c1c
sarastefani <81649211+sarastefani@users.noreply.github.com>
2023-11-17 11:22:56 +0100

    Update alpen_stream_r.py

--- alpen_stream_r.py
+++ alpen_stream_r.py
@@ -177,7 +177,7 @@ with history:
 footerSection = st.container()
 
 
-st.text_input(r"$\textsf{\Large Enter text here}$")
+st.text_input(r"$\textsf{\Large Enter text here}$", key = 'text')
 
     
 #user_question = footerSection.text_input(
